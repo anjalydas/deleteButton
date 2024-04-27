e6840c07988f21ed0e7a011df0888de93b44149c
anjalydas <164157759+anjalydas@users.noreply.github.com>
2024-04-27 14:38:05 +0530

    Your message here

---
+++ index.js
@@ -0,0 +1,7 @@
+const deleteButton = document.getElementById('deleteButton');
+const heading = document.getElementById('heading'); 
+{
+    deleteButton.addEventListener('click', () => {
+heading.remove();
+   })
+}
